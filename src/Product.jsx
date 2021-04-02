094dc653e69828f485ebb7a374a88f1c90874d25
Fergie <akinfergie@gmail.com>
2021-04-02 10:18:35 +0800

    Product Child  Component

---
+++ src/Product.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+  
+class Product extends Component{
+    render(){
+        return (
+            <div className ="col-md-6 col-lg-6">
+                <div className = "card m-2">
+                    <div className="card-body">
+                        <div className="text-muted"># {this.props.product.id}
+                            <span className="float-right hand-icon" onClick={() => {this.props.onDelete(this.props.product)}}>
+                                <i className="fa fa-times"></i>
+                            </span>
+                        </div>
+                        <h5 className="pt-5 border-top">{this.props.product.productName}</h5>
+                        <div> ${this.props.product.price}</div>
+                    </div>
+                    <div className="card-footer" >
+                        <div className="float-left"> 
+                            <span className="badge">{this.props.product.qty}</span>
+                        <div className="btn-group">
+                            <button className="btn btn-outline-success" onClick={ () => {this.props.onIncrement(this.props.product, 10);}}>+</button>
+                            <button className="btn btn-outline-success"  onClick={ () => {this.props.onDecrement(this.props.product, 0);}}>-</button>
+
+                        </div>
+                        </div>
+                        <div className="float-right">
+                            {this.props.children}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        )
+    }
+}
+
+export default Product;
