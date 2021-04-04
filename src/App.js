897a19048e1e8c2a31f43e7a7c5a7dc990e12799
Fergie <akinfergie@gmail.com>
2021-04-04 11:17:51 +0800

    axios for http

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 // import MainContent from "./MainContent";
 import Navbar from './Navbar';
 import Login from './Login';
-// import ShoppingCart from "./ShoppingCart";
+import ShoppingCart from "./ShoppingCart";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
-      <Login/>
+      <ShoppingCart/>
     </React.Fragment>
   );
 }
